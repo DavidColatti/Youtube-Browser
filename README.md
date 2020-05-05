65ef804a5393ed4e940e787f19649cf3953a50b6
DavidColatti <davidcolatti@gmail.com>
2020-05-05 14:57:28 -0400

    video item and list

---
+++ README.md
@@ -0,0 +1,14 @@
+Youtube Browser
+A simple React app that fetches searchable videos using YouTube API
+
+Clone
+Clone this repo to your local machine using https://github.com/DavidColatti/Youtube-Browser.git
+Setup
+Install dependencies with npm install, then npm start and you're ready to view!
+$ npm install
+$ npm start
+View Here - Heroku
+
+
+Built With
+JS, HTML5, CSS3, React
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 const KEY = 'AIzaSyDbQenosmZ2WVau8xS9QAoboO4Tu6u7TCU';
 
 class App extends Component {
+	state = {
+		videos: []
+	};
+
 	onTermSubmit = async (term) => {
 		const res = await youtube.get('/search', {
 			params: {
@@ -15,13 +20,17 @@ class App extends Component {
 				type: 'video'
 			}
 		});
-		console.log(res);
+
+		this.setState({
+			videos: res.data.items
+		});
 	};
 
 	render() {
 		return (
 			<div className="ui container">
 				<SearchBar onFormSubmit={this.onTermSubmit} />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
